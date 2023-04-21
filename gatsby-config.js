c99556c692847b873a9b52e47abb332251ff2c18
A.M <andrej2000aa@gmail.com>
2023-04-21 13:41:29 +0200

    final build # ispravljene sve greske

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,21 @@ module.exports = {
     },
 
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-6L5MCEGDVY", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
+
+    {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: `y6gvwg5f8rqc`,
